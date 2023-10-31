01926ae38b61343bdee105e6b977a1379d369738
Aishwaryajava <128724510+Aishwaryajava@users.noreply.github.com>
2023-10-31 15:29:27 -0400

    Update Ifelse.py

--- Ifelse.py
+++ Ifelse.py
@@ -1,12 +1,12 @@
 # Given an integer, , perform the following conditional actions:
 
-If  is odd, print Weird
-If  is even and in the inclusive range of  to , print Not Weird
-If  is even and in the inclusive range of  to , print Weird
-If  is even and greater than , print Not Weird
+If n is odd, print Weird
+If n is even and in the inclusive range of  to , print Not Weird
+If n is even and in the inclusive range of  to , print Weird
+If n is even and greater than , print Not Weird
 
 #!/bin/python3
-
+#solution 1
 import math
 import os
 import random
@@ -19,6 +19,27 @@ if n % 2!= 0 or (n>=6 and n<=20):
     print("Weird")
 else:
     print("Not Weird")
+    
+#solution 2
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n % 2 != 0:
+        print("Weird")
+    elif 2 <= n <= 5:
+        print("Not Weird")
+    elif 6 <= n <= 20:
+        print("Weird")
+    else:
+        print("Not Weird")
+
 
 
 
