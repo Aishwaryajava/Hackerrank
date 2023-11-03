c3031b66912823f3b0940e2a1b73fb497790b283
Aishwaryajava <128724510+Aishwaryajava@users.noreply.github.com>
2023-11-03 15:45:33 -0400

    Update Ifelse.py

--- Ifelse.py
+++ Ifelse.py
@@ -1,9 +1,9 @@
 # Given an integer, , perform the following conditional actions:
 
-If n is odd, print Weird
-If n is even and in the inclusive range of  to , print Not Weird
-If n is even and in the inclusive range of  to , print Weird
-If n is even and greater than , print Not Weird
+# If n is odd, print Weird
+# If n is even and in the inclusive range of  to , print Not Weird
+# If n is even and in the inclusive range of  to , print Weird
+# If n is even and greater than , print Not Weird
 
 #!/bin/python3
 #solution 1
