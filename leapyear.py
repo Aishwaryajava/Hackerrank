4cc098a55cb808a023aeb007e2bcddab5a463956
Aishwaryajava <128724510+Aishwaryajava@users.noreply.github.com>
2024-02-08 19:51:48 -0500

    Update leapyear.py

--- leapyear.py
+++ leapyear.py
@@ -1,18 +1,16 @@
 def is_leap(year):
     leap = False
-    
-    #if year is divisible by 4 
-    if year % 4 ==0: 
-        leap = True
-    #if year is divisible by 100
-        if year % 100 ==0:
-            leap = True
-    #if year is divisible by 400
-            if year % 400 ==0:
-                leap = True
-    else: 
-        leap = False
-   
+    if year % 4 == 0:  # Check if the year is divisible by 4
+        if year % 100 == 0:  # Check if the year is divisible by 100
+            if year % 400 == 0:  # Check if the year is divisible by 400
+                return True  # Leap year if divisible by 400
+            else:
+                return False  # Not a leap year if divisible by 100 but not by 400
+        else:
+            return True  # Leap year if divisible by 4 but not by 100
+    else:
+        return False  # Not a leap year if not divisible by 4
+
     return leap
 
 year = int(input())
